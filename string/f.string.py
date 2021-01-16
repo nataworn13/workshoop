1b7bc90a28d961bf7d1f0b11da78043a5fd68d22
nataworn13 <nut_nataworn@hotmail.com>
2021-01-16 10:31:50 +0700

    add new file

---
+++ string/f.string.py
@@ -0,0 +1,6 @@
+name = "nut"
+age = 21
+
+result = f" My name is {name}, and I am {age}"
+
+print("result", result)  # output: "My name is nut, and I am 21"
---
+++ string/format.py
@@ -0,0 +1,5 @@
+age = 21
+txt = "My name is nut, and I am {}"
+
+result = txt.format(age)
+print("result:", result)  # output: "My name is nut, and I am 21
