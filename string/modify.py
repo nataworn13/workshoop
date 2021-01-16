77a848d27ac31039e030f877360c3e3f0ca9ccb9
nataworn13 <nut_nataworn@hotmail.com>
2021-01-16 10:20:51 +0700

    add new file

---
+++ string/modify.py
@@ -0,0 +1,8 @@
+string = " Hello, World! "
+
+print(string.upper())  # output: "HELLO, WORLD!"
+print(string.lower())  # output: "hell, world"
+print(string.strip())  # output: "Hello, World!"
+print(string.replace("H", "J"))  # output : Jello, world!
+print(string.split(","))  # output: [' Hello',' world! ']
+print(len(string))  # 15
