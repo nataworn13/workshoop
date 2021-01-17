86c5e58a39f4bca9555faff2cfb1358c3e545876
nataworn13 <nut_nataworn@hotmail.com>
2021-01-17 11:11:33 +0700

    add new file

--- sets/add_sets.py
+++ sets/add_sets.py
@@ -1,9 +1,10 @@
 thisset = {"apple", "banana", "cherry"}
 thisset.add("orange")
 print(thisset)
-
+# output: {'cherry', 'apple', 'banana', 'orange'}
 
 thisset = {"apple", "banana", "cherry"}
 tropical = {"pipneapple", "mango", "papaya"}
 thisset.update(tropical)
 print(thisset)
+# output: {'pipneapple', 'papaya', 'banana', 'mango', 'cherry', 'apple'}
